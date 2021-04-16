f59966326a155729ea5f98eaa3c82baf088ddf3e
pansam996 <pansam9961217@gmail.com>
2021-04-16 11:52:16 +0800

    add some comment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import warnings
+# 有時compile 會報告warning訊息（提醒使用者某些function在最新版不在支援等等，或是他建議的寫法）
+# 增加這一行是將warning的訊息給關閉（前提是你看過warning訊息並且了解後再關閉），
+# 你可以試著將warning 這一段code給註解，看看compile提供給你的warning訊息是什麼
 warnings.filterwarnings('ignore')
+
 
 import numpy as np
 import pandas as pd
@@ -23,6 +27,20 @@ X_train, Y_train = X[:400], Y[:400]     # 訓練資料前400筆
 X_test, Y_test = X[400:], Y[400:]       # 測試資料後107筆
 
 # k-fold, k =10
+# 這一行主要是宣告使用KFold 要切割成幾份
+# 用法如 kf.split(X, Y) <--- 你可以試著 print(kf.split(X, Y)) 這樣子的東西叫做generator
+# 關於 generator 的概念，這邊拿常見的例子來說明
+# for i in range(10) 當中的 range(10) ，就是一個generator，他會隨著for迴圈的進行由 0 ~ 9 每次吐出一個數字
+# 所以 kf.split(X,Y) 也是會執行一樣的動作(隨著for迴圈吐出東西) ，不過他是會吐出兩個東西分別是 train 與 val
+# 所以底下才會這樣子寫 for train, val in kf.split(X,Y): <--- 由於kf.split(X,Y) 是吐出兩個東西所以我們用兩個變數去承接
+# 除此之外你可以試著單獨執行下面這段程式碼
+
+# for train, val in kf.split(X,Y):
+#   print(train)
+#   print(val)
+
+# 你就會發現原來kf.split(X,Y)已經將資料切成k 份，當中k-1份是train ，另外1份是val，並且每次都不同，重複10次
+
 kf = KFold(n_splits = 10)
 
 # 拿來紀錄 val_accuracy的list
@@ -42,11 +60,11 @@ model.summary()
 for train, val in kf.split(X_train, Y_train):
     # 訓練模型
     model.fit(X_train[train], Y_train[train], epochs=120, batch_size=20, verbose=0)
-    
+
     # 計算驗證組準確度
     predict_price = model.predict(X_train[val]).squeeze().tolist()
     predict_price = [round(p, 1) for p in predict_price]
-    
+
     error_range = 2 # 「預測出的房價」與「真實房價」兩者間可以接受的誤差
     result = abs(predict_price - Y_train[val])
     correct = len([x for x in result if x <= error_range])
@@ -59,6 +77,14 @@ print("驗證組平均預測準確度:{:.2f}".format(sum(val_acc)/len(val_acc)))
 
 # 計算測試組準確度
 predict_price = model.predict(X_test).squeeze().tolist()
+# 這種形式的for in 用法等同於以下這段 code:
+# x = []
+# for p in predict_price:
+#   p = round(p, 1)
+#   x.append(p)
+
+# 可以發現 這樣子的寫法 1行可以抵 4行來用
+# 並且這樣子的寫法可以比較簡潔乾淨，之後可以試著往這方向前進
 predict_price = [round(p, 1) for p in predict_price]
 
 error_range = 2 # 可以接受的誤差範圍
